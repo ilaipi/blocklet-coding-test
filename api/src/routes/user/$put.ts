0df223cc14c7b8ac973edcb4475abef79ff8ec3c
Billy Yang <mz.yyam@gmail.com>
2024-06-27 12:08:37 +0800

    feat(api): put user fields validate

--- api/src/routes/user/$put.ts
+++ api/src/routes/user/$put.ts
@@ -4,6 +4,9 @@ import UserDb from "../../models/user";
 
 export default async function $put(req: Request, res: Response) {
   const payload = req.body;
+  if (!payload.username && !payload.email && !payload.phone) {
+    return res.status(400).send('请至少更新一个必填字段！');
+  }
   const updated = await UserDb.update({ _id: req.params.id }, { $set: payload });
 
   return res.json(updated);
